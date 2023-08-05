9ff9ff9d1777ba995d21a1fb4caecd8c20e92cce
Sunarintaroo <128342871+Sunarintaroo@users.noreply.github.com>
2023-08-05 12:45:40 +0530

    Update play.py

--- AsuXMusic/Modules/play.py
+++ AsuXMusic/Modules/play.py
@@ -183,7 +183,7 @@ async def play(c: Abishnoi, m: Message):
                 gcname = m.chat.title
                 videoid = search[4]
                 dlurl = f"https://www.youtubepp.com/watch?v={videoid}"
-                info = f"https://t.me/Exon_Robot?start=info_{videoid}"
+                info = f"https://t.me/Itsukinakanoprobot?start=info_{videoid}"
                 keyboard = stream_markup(user_id, dlurl)
                 playimg = await play_thumb(videoid)
                 queueimg = await queue_thumb(videoid)
